6a52337e52f61a39ab8b0e6cac1aa4c0554d32dd
ryanJohn33 <sendryanperezmail@gmail.com>
2016-10-11 19:51:19 -0500

    First commit

---
+++ Login.swift
@@ -0,0 +1,21 @@
+//
+//  Login.swift
+//  Crave
+//
+//  Created by Ryan Perez on 10/4/16.
+//  Copyright © 2016 Ryan Perez. All rights reserved.
+//
+
+import UIKit
+
+class Login: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    var thing: UIViewContent(asdf)
+}
